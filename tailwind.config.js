a14aa550607dee3d50c4c23986ee76c1f2d3f2a1
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-06-24 01:54:14 +0300

    custom config tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // purge: ['../public/src/**/*.{js,jsx,ts,tsx}', '../public/index.html'],
-  content: [
-    './public/**/*.html',
-    './public/*.html',
 
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
+const colors = {
+    brand: 'hsla(132, 98%, 52%, 1)',
+    light: 'hsla(0, 0%, 100%, 1)',
+    dark: 'hsla(0, 0%, 0%, 1)',
+    dim: 'hsla(0, 0%, 9%, 1)',
+
 }
+
+module.exports = {
+    content: [
+        './public/**/*.html',
+        './public/*.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: colors.brand,
+                light: colors.light,
+                dark: colors.dark,
+                dim: colors.dim,
+            },
+            backgroundImage: {
+                body: `linear-gradient(to bottom right,
+                  ${colors.brand},
+                  ${colors.light},
+                  ${colors.dark},
+                  ${colors.dim})`,
+            },
+
+            fontFamily: {
+                sans: ['Open Sans', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
+
